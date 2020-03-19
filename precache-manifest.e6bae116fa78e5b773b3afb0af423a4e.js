18647be6b5ddf045c496f49ad0c29fda6cb8fbc5
Goh Sheen An <sheen.an.goh@gmail.com>
2020-03-19 12:19:14 +0800

    Updates

---
+++ precache-manifest.e6bae116fa78e5b773b3afb0af423a4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cf06fe3e33cc74d55ac212f76c6bbfe",
+    "url": "/react-box-maker/index.html"
+  },
+  {
+    "revision": "73ece3778c77c1f6b5f4",
+    "url": "/react-box-maker/static/css/main.f210fd9f.chunk.css"
+  },
+  {
+    "revision": "94e13f9d126e12e53c7a",
+    "url": "/react-box-maker/static/js/2.80906204.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-box-maker/static/js/2.80906204.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73ece3778c77c1f6b5f4",
+    "url": "/react-box-maker/static/js/main.8c290367.chunk.js"
+  },
+  {
+    "revision": "9ac4491f4b3ea7f18729",
+    "url": "/react-box-maker/static/js/runtime-main.1a87ba58.js"
+  }
+]);
